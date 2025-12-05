13ca98f48cb5ae76c141e40d6da62c0af4a0582e
kiamehr <100081672+kiyamehr@users.noreply.github.com>
2025-12-05 20:11:07 +0330

    Delete JavaScript/Jonas Schmedtmann's  course codes directory

--- JavaScript/Jonas Schmedtmann's  course codes/01-Fundamentals-Part-1 (current)/starter/01/script.js
+++
@@ -1,6 +0,0 @@
-let kia = "awsome";
-if (kia === "awsome") alert("Kia is Legend Wait For It, DARY! Legendary!!!");
-
-14 + 14 + 230 - 210;
-
-console.log(14 + 14 + 230 - 210);
--- JavaScript/Jonas Schmedtmann's  course codes/01-Fundamentals-Part-1 (current)/starter/02-Variables/script.js
+++
@@ -1,7 +0,0 @@
-console.log(14 + 14 + 230 - 210);
-
-let firstName = "Kiamehr";
-let $lastName = "Moradi";
-
-console.log(firstName);
-console.log($lastName);

d4a08ed67d5c1409627e62106a2b5da4a9ac5fbb
Kiamehr <kiamehrmoradii@gmail.com>
2025-12-05 21:18:30 +0330

    completed the first challenge

---
+++ JavaScript/Jonas Schmedtmann's  course/Assignments & Challenges/Challenges/JavaScript Fundamentals – Part 1/Coding Challenge #1 (BMI)/scripts.js
@@ -0,0 +1,18 @@
+let markWeight = 78;
+const markHeight = 1.69;
+
+let kiamehrWeight = 75;
+const kiamehrHeight = 1.8;
+
+let johnWeight = 92;
+const johnHeight = 1.95;
+
+let markBMI = markWeight / markHeight ** 2;
+
+let johnBMI = johnWeight / johnHeight ** 2;
+
+let kiamehrBMI = kiamehrWeight / kiamehrHeight ** 2;
+
+console.log(markBMI);
+console.log(johnBMI);
+console.log(kiamehrBMI);

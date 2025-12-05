c6024ecf03bd98899eef942b4040fcd10ff36e59
Kiamehr <kiamehrmoradii@gmail.com>
2025-12-05 14:41:19 +0330

    re ordered the files

---
+++ JavaScript/Jonas Schmedtmann's  course codes/01-Fundamentals-Part-1 (current)/starter/03-Data Types/script.js
@@ -0,0 +1,25 @@
+let javascriptIsFun = true;
+
+// console.log(javascriptIsFun);
+
+// typeof: shows the type of a value ----
+console.log(typeof javascriptIsFun);
+
+// console.log(typeof 23);
+// console.log(typeof "hi");
+
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+// undefined variable
+let firstName;
+
+console.log(firstName);
+console.log(typeof firstName);
+
+firstName = "Kiamehr";
+console.log(typeof firstName);
+
+// a bug that js has:
+console.log(typeof null);
+// says that null is a object but is actually just null.

1f34af4b5427a04a03f4b5c9748fb47eb30713a2
Kiamehr <kiamehrmoradii@gmail.com>
2025-12-05 20:10:34 +0330

    did some more re ordering

---
+++ JavaScript/Jonas Schmedtmann's  course/Assignments & Challenges/Assignments/Basic Operators/scripts.js
@@ -0,0 +1,14 @@
+let populationPersia = 90_000_000;
+
+let populationFinLand = 6_000_000;
+
+console.log(populationPersia / 2);
+
+console.log(populationPersia++);
+
+console.log(populationPersia >= populationFinLand);
+
+let description =
+  "Persia" + " " + "has" + " " + populationPersia + " " + "people";
+
+console.log(description);

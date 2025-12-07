48baa4afad3adb8face6c226a8af1660cfebc4d7
Kiamehr <kiamehrmoradii@gmail.com>
2025-12-07 11:53:01 +0330

    started fundamentals part 2

---
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/02-Fundamentals-Part-2/starter/01-Activating strictmode/script.js
@@ -0,0 +1,12 @@
+// We will allways turn this on!!!
+"use strict";
+
+let hasDriversLicence = false;
+const passTest = true;
+
+// i have a typo here and cause use strict is on it will tell me about it otherwise i wouldnt notice it
+if (passTest) hasDriverLicence = true;
+
+hasDriversLicence
+  ? console.log("I Can Drive :D")
+  : console.log("I Can't Drive :(");

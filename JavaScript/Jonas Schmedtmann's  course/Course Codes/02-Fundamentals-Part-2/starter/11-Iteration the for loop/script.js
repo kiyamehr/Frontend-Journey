1dd821a0dfc4b84fad28ada8433f083cbf1b1fe7
Kiamehr <kiamehrmoradii@gmail.com>
2025-12-09 09:57:48 +0330

    learned about loops and for loop

---
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/02-Fundamentals-Part-2/starter/11-Iteration the for loop/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// console.log("I studied for 1 Minute...");
+// console.log("I studied for 2 Minute...");
+// console.log("I studied for 3 Minute...");
+// console.log("I studied for 4 Minute...");
+// console.log("I studied for 5 Minute...");
+
+// in a loop:
+
+// The variable      The Condition      The Task
+for (let timeStudy = 1; timeStudy <= 10; timeStudy++) {
+  console.log(`I have Studied For ${timeStudy} Minutes`);
+}

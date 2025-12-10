2252f8b9ca10550930112201c3e35d782edd38c3
Kiamehr <kiamehrmoradii@gmail.com>
2025-12-10 18:07:12 +0330

    learned changing and getting value from html elements

---
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/05-Guess-My-Number/starter/02-Selecting and manipulating elements/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+document.querySelector(".message").textContent =
+  "Hi I Was Selected and changed";
+
+document.querySelector(".number").textContent = "18";
+
+document.querySelector(".label-score").textContent = "992";
+
+// get value from input
+document.querySelector(".guess").value = 20;
+console.log(document.querySelector(".guess").value);

cfe624f0b1f8fb5559581614c31c764d1a0acd2e
Kiamehr <kiamehrmoradii@gmail.com>
2025-12-11 23:14:52 +0330

    learend adding and removing classes with js

---
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/06-Modal/starter/02-Working with classes/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+
+// Works like an Array cause we selected all the elements
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+// since it works like an array we can simply iterate through it
+
+const showModal = function () {
+  // // how to click
+  // console.log(btnOpenModal[i].textContent);
+
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", showModal);
+}
+
+// we wont add () cause we dont want to js immidietly run the function
+btnCloseModal.addEventListener("click", closeModal); // NO "()" Cause we arent calling the function
+overlay.addEventListener("click", closeModal);
+console.log("fuck");

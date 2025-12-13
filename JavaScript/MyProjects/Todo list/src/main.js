c442772e0b0b6462bbbc0666afee31f8ab1ea548
Kiamehr <kiamehrmoradii@gmail.com>
2025-12-13 11:46:59 +0330

    added js selectors for elements

---
+++ JavaScript/MyProjects/Todo list/src/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Definings elements inside classes
+
+const inputTaskEl = document.getElementById("input-task");
+const btnAddTask = document.getElementById("add-task");
+
+const btnCheckBox = document.querySelectorAll(".checkbox");
+const btncheckBoxLabel = document.querySelectorAll(".checkbox-label");
+
+const btnDelete = document.querySelectorAll(".delete-icon");
+const btnEdit = document.querySelectorAll(".edit-icon");

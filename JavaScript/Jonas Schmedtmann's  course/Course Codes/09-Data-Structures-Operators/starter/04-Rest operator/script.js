42697deb59e7cec49733105839a053a64d9eaece
Kiamehr <kiamehrmoradii@gmail.com>
2025-12-16 22:14:43 +0330

    learned about Rest op and its diffrence with spread

---
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/09-Data-Structures-Operators/starter/04-Rest operator/script.js
@@ -0,0 +1,135 @@
+'use strict';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterOrderIndex, mainOrderIndex) {
+    return [this.starterMenu[starterOrderIndex], this.mainMenu[mainOrderIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  createPasta: function (ing1, ing2, ing3) {
+    return `Here is your pasta with ${ing1}, ${ing2}, ${ing3}, `;
+  },
+
+  orderPizza: function (mainIngredirent, ...otherIngs) {
+    return `You Pizza's Main Ingredient is ${mainIngredirent}, and you have also ${otherIngs} in your pizza`;
+  },
+};
+
+// * Destructing
+
+// spread is on the rightside of =
+// rest is on the leftside of =
+const [a, b, ...other] = [1, 2, 3, 4, 5];
+console.log(a, b, other);
+
+const [pizza, , risotto, ...otherFoods] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFoods);
+
+//* Objects
+
+const { sat, ...weekDays } = { ...restaurant.openingHours };
+
+console.log(sat, weekDays);
+
+// * Functions
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+};
+
+console.log(add(1, 2, 3, 4));
+
+console.log(add(1, 2, 19));
+console.log(
+  add(
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1
+  )
+);
+
+const numArray = [1, 2, 3, 4, 5];
+console.log(add(...numArray));
+
+console.log(restaurant.orderPizza('wurst', 'hanchen', ' schwein'));

c14faec985bb7586ea5e60e0c49ffb2d08e58118
Kiamehr <kiamehrmoradii@gmail.com>
2025-12-20 16:25:35 +0330

    learned the bind method

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/10-Functions/starter/05-The call and apply methods/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/10-Functions/starter/05-The call and apply methods/script.js
@@ -41,3 +41,52 @@ book.apply(eurowings, flightData);
 console.log(eurowings);
 
 book.call(eurowings, ...flightData); // does the same thing as apply
+
+// Bind Method
+
+const bookLH = book.bind(lufthansa);
+const bookEW = book.bind(eurowings);
+
+bookEW(20, 'kiamehr moradi');
+
+// You can even preset values
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('jadi jadi');
+bookEW23('Jonas Schmedtmann');
+
+console.log('------------');
+
+// with event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+// lufthansa.buyPlane();
+
+// since the 'this' keyword will return to the element its attached to
+// when attached to a event lister it will return NaN, so we use bind
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application (presetting valeus)
+const addTax = (rate, value) => value + value * rate;
+const addTaxVAT = addTax.bind(null, 0.23);
+console.log(addTaxVAT(200));
+
+// coding it with functions calling other functions
+// method 1
+// const addTax = rate => value => value + value * rate;
+// const addTaxVAT = addTax(0.23);
+
+// method 2
+// const addTax = function(rate) {
+//   return function(value) {
+//     return value + value * rate
+//   }
+// }
+
+console.log(addTaxVAT(200));

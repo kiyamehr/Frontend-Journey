a8ca9fb2463905a7745ace9a60dbde6a20e79da3
Kiamehr <kiamehrmoradii@gmail.com>
2026-01-04 21:17:06 +0330

    learned more about style mehtods and attributs adn classes

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/13-Advanced-DOM-Bankist/starter/03-Styles Attributes and Classes/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/13-Advanced-DOM-Bankist/starter/03-Styles Attributes and Classes/script.js
@@ -34,4 +34,56 @@ document.addEventListener('keydown', function (e) {
 ///////////////////////////////////////
 ///////////////////////////////////////
 ///////////////////////////////////////
-//* Lectures
+const header = document.querySelector('.header');
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+header.append(message);
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // message.remove();
+
+    message.parentElement.removeChild(message);
+  });
+
+//* Styles
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+// document.documentElement.style.setProperty();
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+
+console.log(logo.alt);
+console.log(logo.src);
+
+// Non-Standard
+console.log(logo.designer);
+
+console.log(logo.getAttribute('designer')); // get none standard attributes
+
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data Attribute
+console.log(logo.dataset.versionNumber);

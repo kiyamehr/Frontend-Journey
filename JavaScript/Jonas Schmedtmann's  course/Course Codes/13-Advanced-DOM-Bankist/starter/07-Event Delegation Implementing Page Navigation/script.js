3b4152e727ec66c9e87727c2e80641ff5414fa09
Kiamehr <kiamehrmoradii@gmail.com>
2026-01-05 10:37:49 +0330

    started  session 7

---
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/13-Advanced-DOM-Bankist/starter/07-Event Delegation Implementing Page Navigation/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach((_, i) => {
+  btnsOpenModal[i].addEventListener('click', openModal);
+});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+///////////////////////////////////////
+
+///////////////////////////////////////
+
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+});
+
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////

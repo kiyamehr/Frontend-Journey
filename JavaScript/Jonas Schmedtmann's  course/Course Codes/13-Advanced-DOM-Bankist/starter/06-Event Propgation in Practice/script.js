bedbbde53ef70a7d1b89272536f80ee1e49c3b11
Kiamehr <kiamehrmoradii@gmail.com>
2026-01-05 10:31:50 +0330

    leanred about capture, target, and bubble phases as a tree in DOM

---
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/13-Advanced-DOM-Bankist/starter/06-Event Propgation in Practice/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach((_, i) => {
+  btnsOpenModal[i].addEventListener('click', openModal);
+});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+
+const randomInt = (min, max) => Math.trunc(Math.random() * (max - min + 1));
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+
+  // this is the same as e.current target
+  console.log(e.currentTarget === this);
+});
+
+document.querySelector('.nav__links').addEventListener('click', function () {
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector('.nav').addEventListener('click', function () {
+  this.style.backgroundColor = randomColor();
+});

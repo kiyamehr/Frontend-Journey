33c8d7f9ba29fd2eaa0e1c90b52fddc49932673a
Kiamehr <kiamehrmoradii@gmail.com>
2026-01-05 09:48:43 +0330

    learned more about event listeneres

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/13-Advanced-DOM-Bankist/starter/05-Types of Events and Event Handlers/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/13-Advanced-DOM-Bankist/starter/05-Types of Events and Event Handlers/script.js
@@ -34,3 +34,16 @@ document.addEventListener('keydown', function (e) {
 ///////////////////////////////////////
 ///////////////////////////////////////
 ///////////////////////////////////////
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function () {
+  alert('addEventListener Great you hovered!');
+
+  // removing the event listener
+  // h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);

153fd986b5e1f1dbd900d223e6aa9c660425ecf5
Kiamehr <kiamehrmoradii@gmail.com>
2026-02-08 21:20:18 +0330

    learned about how to use prototyes and what they do

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/02-Prototypes/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/02-Prototypes/script.js
@@ -5,16 +5,35 @@ const Person = function (firstName, age) {
   this.age = age;
 
   //! Never Do This!
-  this.calcAge = function () {
-    console.log(2025 - this.age);
-  };
+  // this.calcAge = function () {
+  //   console.log(2025 - this.age);
+  // };
 };
 
-const kiamehr = new Person('Kiamehr', '18');
-const mmd = new Person('mmd', '150');
+console.log(Person.prototype);
 
-console.log(kiamehr);
-console.log(mmd);
-console.log(kiamehr instanceof Person);
+//? Do This
+// Prototypal Inhertense
+Person.prototype.calcAge = function () {
+  console.log(2025 - this.age);
+};
+
+const kiamehr = new Person('kiamehr', 18);
 
 kiamehr.calcAge();
+
+// To check an objects prototypes
+console.log(kiamehr.__proto__);
+console.log(kiamehr.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(kiamehr));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// more honest name since its not a prototype of the class itself: \n
+//? .prototypeOfLinkedObjects
+
+// defining attribute using prototypes
+Person.prototype.species = 'Homo Sapiens';
+
+console.log(kiamehr.species);
+
+kiamehr.hasOwnProperty('firstName');

c60a32b04b3a72ea3c2eebd150618f1199026a96
Kiamehr <kiamehrmoradii@gmail.com>
2026-02-08 20:52:21 +0330

    learned how the create a class and use the new operator

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/01-Constructor functions and the new operator/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/01-Constructor functions and the new operator/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const Person = function (firstName, age) {
+  this.firstName = firstName;
+  this.age = age;
+
+  //! Never Do This!
+  this.calcAge = function () {
+    console.log(2025 - this.age);
+  };
+};
+
+const kiamehr = new Person('Kiamehr', '18');
+const mmd = new Person('mmd', '150');
+
+console.log(kiamehr);
+console.log(mmd);
+console.log(kiamehr instanceof Person);
+
+kiamehr.calcAge();

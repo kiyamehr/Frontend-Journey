63824b28d8af14d20c1b3d8f7a58f3c852c93468
Kiamehr <kiamehrmoradii@gmail.com>
2026-02-09 13:18:52 +0330

    learned about getter and setters

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/05-Setters and Getters/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/05-Setters and Getters/script.js
@@ -7,8 +7,8 @@
 
 // Class Decleration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -20,27 +20,48 @@ class PersonCl {
   greet() {
     return `Hello ${this.firstName}`;
   }
+
+  //? Is actually Added as a property
+  get age() {
+    return 2025 - this.birthYear;
+  }
+
+  // Set a Property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert('name is not a fullname!');
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-// PersonCl.prototype.greet = function(){
-//   return `Hello ${this.firstName}`
-// }
+const kia = new PersonCl('Kiamehr Moradi', 2007);
 
-const kia = new PersonCl('Kiamehr', 2007);
+// Setters and Getters on Objects
+const kiaAccount = {
+  firstName: 'Kiamehr',
+  movements: [20, 100, 300, 500, 42],
 
-console.log(kia.calcAge());
+  // when you need to get something out of an array with some calculations before
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
 
-console.log(PersonCl); // to see the class itself
+  // when you want to set something to an object
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
 
-console.log(kia.__proto__ === PersonCl.prototype);
+console.log(kiaAccount.latest);
 
-console.log(kia.greet());
+kiaAccount.latest = 50;
+console.log(kiaAccount.movements);
 
-/*
+// using get and set in classes
+console.log(kia.age);
 
-Notes About Classes
-1. They are Not Hoisted: can't use em before they are defined
-2. Allways run in strict mode
-3. Classes are first-class Citizens
-
-*/
+// kia.fullname = 'Kiamehr';
+console.log(kia.fullName);

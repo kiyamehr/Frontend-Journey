e0fead7cabc6b8bfc6e61e442dac7f6030a91be3
Kiamehr <kiamehrmoradii@gmail.com>
2026-02-09 19:32:29 +0330

    learned how inhertience work with constructor functions

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/08-Inheritance Between Classes Constructor Functions/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/08-Inheritance Between Classes Constructor Functions/script.js
@@ -1,32 +1,35 @@
 'use strict';
 
-const PersonProto = {
-  calcAge: function () {
-    return 2026 - this.birthYear;
-  },
-
-  firstWord: function () {
-    return this.name[0];
-  },
-
-  init(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  },
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
 };
 
-const kia = Object.create(PersonProto);
-console.log(kia);
+Person.prototype.calcAge = function () {
+  return 2026 - this.birthYear;
+};
 
-// when we use constructor funcitons the 'new' keyword does prototype and property syncing \\
-//  automaticaly for us
-kia.name = 'Kiamehr';
-kia.birthYear = 2007;
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// If we do this after line 20 then object.create would overwrite \n
+// the intruduce prototype, cause object.create creates an empty object.
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  return `Hello, I'm ${this.firstName}, and i am studying ${this.course}`;
+};
+
+const kia = new Student('Kiamehr', 2007, 'Computer Engneering');
+
+// So the Constructor Relies on the Student itself
+Student.prototype.constructor = Student;
+
+console.log(kia instanceof Student);
+console.log(kia instanceof Person);
+console.log(kia instanceof Object);
+
+console.log(kia.introduce());
 console.log(kia.calcAge());
-console.log(kia.firstWord());
-
-const jadi = Object.create(PersonProto);
-
-jadi.init('Jadi Mir Mir', 1978);
-
-console.log(jadi.calcAge());

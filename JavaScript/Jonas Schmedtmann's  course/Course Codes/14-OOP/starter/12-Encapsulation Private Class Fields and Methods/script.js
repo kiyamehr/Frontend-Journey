bc914d06fd664d4f6447fb9c2826f5671380dca9
Kiamehr <kiamehrmoradii@gmail.com>
2026-02-10 12:15:20 +0330

    learned how to capsulate and make public fields and private fields and methods

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/12-Encapsulation Private Class Fields and Methods/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/12-Encapsulation Private Class Fields and Methods/script.js
@@ -1,31 +1,43 @@
 'use strict';
 
 class PersonCl {
+  // Public Fields //? (accessed on all instances)
+  locale = navigator.language;
+
+  // Private Fields //? (accessed only inside class)
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+  }
+
+  // Public Interface (API)
+  getMovements() {
+    return this.#movements;
   }
 
   deposit(val) {
-    this.movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approvedLoan() {
+  // '#' made this funciton Private
+  #approvedLoan() {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approvedLoan()) this.deposit(val);
+    if (this.#approvedLoan()) this.deposit(val);
   }
 }
 
 const kiaAcc = new PersonCl('Kiamehr', 'IRR', '1111');
 
 kiaAcc.requestLoan(1000);
+console.log(kiaAcc);

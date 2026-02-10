bc3998ddfb3551abfcc10a96cc85cfdc001f3ddd
Kiamehr <kiamehrmoradii@gmail.com>
2026-02-10 11:32:39 +0330

    learned how to create internal properties without needing to get them from users

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/11-Another Class Example/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/14-OOP/starter/11-Another Class Example/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+class PersonCl {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+  }
+
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approvedLoan() {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approvedLoan()) this.deposit(val);
+  }
+}
+
+const kiaAcc = new PersonCl('Kiamehr', 'IRR', '1111');
+
+kiaAcc.requestLoan(1000);

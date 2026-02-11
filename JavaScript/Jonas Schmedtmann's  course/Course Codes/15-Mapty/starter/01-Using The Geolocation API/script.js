689a13b87cf913f93966d0ca023b42760c523b8e
Kiamehr <kiamehrmoradii@gmail.com>
2026-02-11 20:07:16 +0330

    learned about getcurrentposition and so much more about the geolocation API!

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/15-Mapty/starter/01-Using The Geolocation API/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/15-Mapty/starter/01-Using The Geolocation API/script.js
@@ -10,3 +10,22 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// If the user's browser supported geolocation (old browsers don't)
+if (navigator.geolocation) {
+  // getting user's current location
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      //? First callback function: when success (could get the location)
+
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      //? Second callback function: when fail (could not get the location)
+      alert('Could Not Get Your Location!');
+    },
+  );
+}

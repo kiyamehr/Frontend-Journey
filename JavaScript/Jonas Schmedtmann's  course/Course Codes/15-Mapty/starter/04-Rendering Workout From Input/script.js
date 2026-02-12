6408f5b54af979d19407cd684ba08a3a0b4860c9
Kiamehr <kiamehrmoradii@gmail.com>
2026-02-12 12:24:22 +0330

    implemented opening the inputs while cliking on the map for adding an excersise

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/15-Mapty/starter/04-Rendering Workout From Input/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/15-Mapty/starter/04-Rendering Workout From Input/script.js
@@ -4,12 +4,16 @@
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
+const FormInputs = document.querySelectorAll('.form__input');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Global Variables
+let map, mapEvent;
 
 // If the user's browser supported geolocation (old browsers don't)
 if (navigator.geolocation) {
@@ -24,7 +28,7 @@ if (navigator.geolocation) {
       //* Leaflet Map
 
       const coords = [latitude, longitude];
-      const map = L.map('map').setView(coords, 15);
+      map = L.map('map').setView(coords, 15);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
@@ -32,21 +36,11 @@ if (navigator.geolocation) {
       }).addTo(map);
 
       //? Leaflet event Listener on map
-      map.on('click', function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng], { riseOnHover: true })
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxwidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            }),
-          )
-          .setPopupContent('Workout')
-          .openPopup();
+      map.on('click', function (mapE) {
+        // Giving the event to a global variable
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
 
       // console.log(`https://www.google.com/maps/@${longitude},${latitude}`);
@@ -57,3 +51,37 @@ if (navigator.geolocation) {
     },
   );
 }
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  inputDistance.value =
+    inputCadence.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+
+  // Displaying Map Marker
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng], { riseOnHover: true })
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxwidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      }),
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+
+  form.classList.add('hidden');
+});
+
+inputType.addEventListener('change', function () {
+  // This way allways one is hidden and one is showing
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputDistance.closest('.form__row').classList.toggle('form__row--hidden');
+});

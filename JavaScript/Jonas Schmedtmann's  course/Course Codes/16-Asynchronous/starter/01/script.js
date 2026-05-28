83e588650f1b39fb1d45fe962bb35c720a0120f9
Kiamehr <kiamehrmoradii@gmail.com>
2026-05-28 09:27:33 +0330

    started asychronous JavaScript

--- JavaScript/Jonas Schmedtmann's  course/Course Codes/16-Asynchronous/starter/01/script.js
+++ JavaScript/Jonas Schmedtmann's  course/Course Codes/16-Asynchronous/starter/01/script.js
@@ -10,3 +10,11 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+
+request.open('GET', 'https://restcountries.com/v2/name/portugal');
+
+request.addEventListener('load', function () {
+  request.send(this.responseText);
+});
